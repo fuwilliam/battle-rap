bd33e4ea86308d155be42684f5f4241936b200c2
fuwilliam <williamfuu@gmail.com>
2022-10-18 15:55:15 -0300

    airflow webserver wont run lmao

--- airflow/dags/scripts/load_rappers.py
+++ airflow/dags/scripts/load_rappers.py
@@ -3,6 +3,7 @@
 import os
 from dotenv import load_dotenv
 from scripts.artist_lister import ArtistLister
+import scripts.spotify_dicts
 
 import pandas as pd
 import time
@@ -16,23 +17,8 @@ client_id = os.getenv('SPOTIFY_CLIENT_ID')
 client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
 sqlalchemy_conn = os.getenv('POSTGRES_CONN')
 
-genreDict = {
-    #"rap": 150,
-    # "hip hop": 150,
-    # "alternative hip hop": 50,
-    # "escape room": 50,
-    # "drill": 30,
-    # "grime": 20,
-    "pluggnb": 20
-}
-
-playlistDict = {
-    # "37i9dQZF1DX0XUsuxWHRQd": "Rap Caviar",
-    # "37i9dQZF1DWY4xHQp97fN6": "Get Turnt",
-    # "37i9dQZF1DWTggY0yqBxES": "Alternative Hip Hop",
-    # "37i9dQZF1DX9oh43oAzkyx": "Beast Mode Hip Hop",
-    # "37i9dQZF1DX186v583rmzp": "90s Hip Hop"
-}
+genreDict = scripts.spotify_dicts.genreDict
+playlistDict = scripts.spotify_dicts.playlistDict
 
 def compile_artists(lister, genreDict, playlistDict):
     print('Compiling artist list...')
---
+++ airflow/dags/scripts/spotify_dicts.py
@@ -0,0 +1,17 @@
+genreDict = {
+    #"rap": 150,
+    # "hip hop": 150,
+    # "alternative hip hop": 50,
+    # "escape room": 50,
+    # "drill": 30,
+    # "grime": 20,
+    "pluggnb": 20
+}
+
+playlistDict = {
+    # "37i9dQZF1DX0XUsuxWHRQd": "Rap Caviar",
+    # "37i9dQZF1DWY4xHQp97fN6": "Get Turnt",
+    # "37i9dQZF1DWTggY0yqBxES": "Alternative Hip Hop",
+    # "37i9dQZF1DX9oh43oAzkyx": "Beast Mode Hip Hop",
+    # "37i9dQZF1DX186v583rmzp": "90s Hip Hop"
+}
