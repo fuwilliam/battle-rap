02fc5f0a1ac36183c5a04740b24bb961983cc56e
williamfuu@gmail.com <williamfuu@gmail.com>
2022-10-20 15:52:46 +0000

    finished staging models and added final rapper model

---
+++ airflow/dbt_models/models/battle-rap/top_tracks.sql
@@ -0,0 +1,9 @@
+SELECT 
+    artist_id,
+    track_rank,
+    track_name,
+    track_id,
+    track_url,
+    preview_url,
+    load_date
+FROM {{ ref('stg_top_tracks') }}
--- airflow/dbt_models/models/staging/stg_rappers.sql
+++ airflow/dbt_models/models/staging/stg_rappers.sql
@@ -3,7 +3,6 @@ SELECT
     artist_name,
     popularity,
     followers,
-    genres,
     image_url,
     DATETIME((TIMESTAMP_SECONDS(CAST(load_date AS INT64))), 'UTC') AS load_date
 FROM {{ source('raw', 'rappers') }}
